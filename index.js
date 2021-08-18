63d05e52ce9923f3da706a04af4b831065ceafc8
Aoi <ht.so.ik0102@gmail.com>
2021-08-18 07:55:34 +0700

    final.txt

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const fs = require("fs");
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     // console.log(data1);
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log("Your life has been written!!");
+            })
+        })
     })
 });
