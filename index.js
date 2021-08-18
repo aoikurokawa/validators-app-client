0d026bb569e75fbc2c89e0e3a6ededdc9b05451b
Aoi <ht.so.ik0102@gmail.com>
2021-08-18 07:50:05 +0700

    calback

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 
 // Blocking synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log("FIle written!");
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log("FIle written!");
 
 // non-blockcing asynchrous 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    console.log(data);
-})
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    // console.log(data1);
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+    })
+});
