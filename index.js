ffe8f7aa53440bf3a9a5b8b3a92464e8321ab160
Aoi <ht.so.ik0102@gmail.com>
2021-08-18 17:32:14 +0700

    synchronous data

--- index.js
+++ index.js
@@ -31,22 +31,29 @@ const url = require("url");
 
 
 //////////////////////////////////////////
+// server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
 
     const pathName = req.url;
     if (pathName === "/overview") {
         res.end("This is the overview");
-    } else if (pathName === "/product"){
+    } else if (pathName === "/product") {
         res.end("THis is the PRODUCT");
+    } else if (pathName === "/api") {
+        res.writeHead(200, {"Content-type": "application/json"});
+        res.end(data);
     } else {
         res.writeHead(404, {
-            "Content-type": 'text/html', 
+            "Content-type": 'text/html',
             "my-own-header": "hello-world",
 
         });
         res.end("<h1>Page not found!</h1>");
     }
-    
+
 });
 
 server.listen(8000, '127.0.0.1', () => {
